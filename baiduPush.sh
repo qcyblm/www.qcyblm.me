f42e6afda9b2a422ee16f730eee4780f74639111
qcyblm <qcyblm@outlook.com>
2020-11-30 20:58:53 +0800

    Update baiduPush.sh

--- baiduPush.sh
+++ baiduPush.sh
@@ -5,5 +5,3 @@ set -e
 
 # 百度链接推送
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://qcyblm.gitee.io&token=0JE7IqrcilaX5f3G"
-
-rm -rf urls.txt # 灭迹
