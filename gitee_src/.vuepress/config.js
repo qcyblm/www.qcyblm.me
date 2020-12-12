54432e881ed8340c1b8d26b13ef45608a9689815
qcyblm <qcyblm@outlook.com>
2020-12-12 19:22:02 +0800

    add lastUpdated: 2020-12-12 11:22:01 GMT

--- gitee_src/.vuepress/config.js
+++ gitee_src/.vuepress/config.js
@@ -1,5 +1,19 @@
 const moment = require('moment')
-moment.locale('zh-cn')
+moment.updateLocale('zh-cn', {
+  meridiem : function (hour, minute) {
+      if (hour < 9) {
+          return "早上";
+      } else if (hour < 11 && minute < 30) {
+          return "上午";
+      } else if (hour < 13 && minute < 30) {
+          return "中午";
+      } else if (hour < 18) {
+          return "下午";
+      } else {
+          return "晚上";
+      }
+  }
+});
 module.exports = {
   title: '青春永不落幕',
   description: '一位静态网站设计爱好者',
