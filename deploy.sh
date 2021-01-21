95322ef2b4bafc6fbe159b643e89d525a12e0789
qcyblm <qcyblm@outlook.com>
2021-01-21 20:59:32 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,10 @@ echo "module.exports={clientId: '${VSSUE_CLIENTID}',clientSecret: '${VSSUE_CLIEN
 yarn build
 
 cd src/.vuepress/dist
-
-if
-  msg='来自GitHub Action的自动部署'
-  githubUrl=git@github.com:qcyblm/qcyblm.github.io.git
-  git config --global user.name "${GITHUB_NAME}"
-  git config --global user.email "${GITHUB_EMAIL}"
-fi
+msg='来自GitHub Action的自动部署'
+githubUrl=git@github.com:qcyblm/qcyblm.github.io.git
+git config --global user.name "${GITHUB_NAME}"
+git config --global user.email "${GITHUB_EMAIL}"
 git init
 git add -A
 git commit -m "${msg}：$(TZ="Asia/Shanghai" date +"%Y-%m-%d %T %Z")"
