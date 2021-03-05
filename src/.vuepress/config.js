524ee13e58130a6e3b79ec66faed9c115a0ffe61
青春永不落幕 <qcyblm@139.com>
2021-03-05 17:34:38 +0800

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -63,7 +63,7 @@ module.exports = {
       transformer: (timestamp, lang) => {
         const moment = require('moment')
         moment.locale(lang)
-        return moment(timestamp).format("YYYY-MM-DD HH-mm")
+        return moment(timestamp).format("YYYY-MM-DD HH:mm")
       }
     }
   }
